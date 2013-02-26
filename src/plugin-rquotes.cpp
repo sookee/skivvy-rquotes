8878c7b0f52ac12048e1b11eeafa01d54917b544
sookee <oasookee@gmail.com>
2013-02-26 01:05:10 +0000

    Moved to libsookee for log.h & bug.h

--- src/plugin-rquotes.cpp
+++ src/plugin-rquotes.cpp
@@ -41,6 +41,7 @@ http://www.gnu.org/licenses/gpl-2.0.html
 #include <skivvy/str.h>
 #include <skivvy/types.h>
 #include <skivvy/logrep.h>
+#include <skivvy/utils.h>
 #include <skivvy/store.h>
 
 namespace skivvy { namespace rquotes {
